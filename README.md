24b9206529c28b6aebaa5d7167f40543222a81f9
Emma <>
2016-12-15 17:44:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on December 15, 2016, 4:32 pm.
---
+++ src/AppBundle/RadditAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Raddit\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RadditAppBundle extends Bundle
+{
+}
