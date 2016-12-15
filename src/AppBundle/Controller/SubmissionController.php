3a2b55d2c196ced0ff9f5e27d8e0be90e8c62014
Emma <>
2016-12-15 20:17:06 +0100

    move views into bundle namespace

--- src/AppBundle/Controller/SubmissionController.php
+++ src/AppBundle/Controller/SubmissionController.php
@@ -4,16 +4,19 @@ namespace Raddit\AppBundle\Controller;
 
 use Raddit\AppBundle\Entity\Submission;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 final class SubmissionController extends Controller {
     /**
      * View submissions on the front page.
+     *
+     * @return Response
      */
     public function frontPageAction() {
         $submissions = $this->getDoctrine()->getRepository(Submission::class)
             ->findBy([], ['id' => 'DESC'], 20);
 
-        return $this->render('front.html.twig', [
+        return $this->render('@RadditApp/front.html.twig', [
             'submissions' => $submissions,
         ]);
     }
