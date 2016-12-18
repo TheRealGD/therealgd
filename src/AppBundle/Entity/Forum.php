252de209720d4b1c02e4eda71fa3afd3cf591b8b
Emma <>
2016-12-18 15:07:43 +0100

    add missing UniqueEntity validator

--- src/AppBundle/Entity/Forum.php
+++ src/AppBundle/Entity/Forum.php
@@ -5,6 +5,7 @@ namespace Raddit\AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -12,6 +13,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Entity()
  * @ORM\Table(name="forums")
+ *
+ * @UniqueEntity({"name"})
  */
 class Forum {
     /**
