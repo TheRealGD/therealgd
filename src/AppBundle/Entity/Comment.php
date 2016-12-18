100339a18f69501bf37e8b28935eaacc791d5fbf
Emma <>
2016-12-18 15:29:38 +0100

    add comment validators

--- src/AppBundle/Entity/Comment.php
+++ src/AppBundle/Entity/Comment.php
@@ -5,6 +5,7 @@ namespace Raddit\AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity()
@@ -22,6 +23,9 @@ class Comment {
 
     /**
      * @ORM\Column(type="text")
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(max=10000)
      *
      * @var string
      */
