e4fe8bdbafd2cffc94801eec5be7dec7cade6256
Emma <>
2016-12-18 15:25:36 +0100

    ability to create top-level comments

--- src/AppBundle/Controller/SubmissionController.php
+++ src/AppBundle/Controller/SubmissionController.php
@@ -44,18 +44,43 @@ final class SubmissionController extends Controller {
     }
 
     /**
-     * Show a submission's comment page.
+     * Show a submission's comment page. The form creates a new top-level
+     * comment.
      *
      * @ParamConverter("forum", options={"mapping": {"forum_name": "name"}})
      * @ParamConverter("submission", options={"mapping": {"forum": "forum", "submission_id": "id"}})
      *
      * @param Forum      $forum
      * @param Submission $submission
+     * @param Request    $request
      *
      * @return Response
      */
-    public function commentPageAction(Forum $forum, Submission $submission) {
+    public function commentPageAction(Forum $forum, Submission $submission, Request $request) {
+        $comment = new Comment();
+
+        // todo - only show comment form for logged-in users
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setSubmission($submission);
+            $comment->setUser($this->getUser());
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($comment);
+            $em->flush();
+
+            return $this->redirectToRoute('raddit_app_comment', [
+                'forum_name' => $forum->getName(),
+                'submission_id' => $submission->getId(),
+                'comment_id' => $comment->getId(),
+            ]);
+        }
+
         return $this->render('@RadditApp/comments.html.twig', [
+            'form' => $form->createView(),
             'submission' => $submission,
         ]);
     }
@@ -83,6 +108,7 @@ final class SubmissionController extends Controller {
     ) {
         $reply = new Comment();
 
+        // todo - only show comment form for logged-in users
         $form = $this->createForm(CommentType::class, $reply);
         $form->handleRequest($request);
 
@@ -95,6 +121,12 @@ final class SubmissionController extends Controller {
 
             $em->persist($reply);
             $em->flush();
+
+            return $this->redirectToRoute('raddit_app_comment', [
+                'forum_name' => $forum->getName(),
+                'submission_id' => $submission->getId(),
+                'comment_id' => $reply->getId(),
+            ]);
         }
 
         return $this->render('@RadditApp/comment.html.twig', [
