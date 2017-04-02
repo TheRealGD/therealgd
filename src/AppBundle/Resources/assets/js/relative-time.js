16932919a9b1bd08c724c312d73a25bbc37b02ec
Emma <emma1312@protonmail.ch>
2017-04-02 09:16:58 +0200

    fix a nasty infinite recursion bug in JS

--- src/AppBundle/Resources/assets/js/relative-time.js
+++ src/AppBundle/Resources/assets/js/relative-time.js
@@ -1,12 +1,9 @@
 'use strict';
 
-import $ from 'jquery';
 import moment from 'moment';
 
-export default function (root) {
-    root = root || ':root';
-
-    $(root).find('.relative-time[datetime]').each(function () {
+export default function ($) {
+    $('.relative-time[datetime]').each(function () {
         const isoTime = $(this).attr('datetime');
 
         $(this).text(moment(isoTime).fromNow());
