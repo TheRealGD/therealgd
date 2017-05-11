a2ceee91c207a82dddc079da80d929859aeb56ce
Emma <emma1312@protonmail.ch>
2017-05-11 16:12:29 +0200

    added spanish translation (thanks, sancocho)

--- src/AppBundle/Form/UserSettingsType.php
+++ src/AppBundle/Form/UserSettingsType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Intl\Intl;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class UserSettingsType extends AbstractType {
-    const LOCALES = ['en', 'nb'];
+    const LOCALES = ['en', 'es', 'nb'];
 
     /**
      * {@inheritdoc}
