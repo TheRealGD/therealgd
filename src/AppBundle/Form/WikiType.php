9584b2055a405a6b916c30e300dd3f8efdff6680
Emma <emma1312@protonmail.ch>
2017-06-12 07:50:13 +0200

    use correct type for wiki body

--- src/AppBundle/Form/WikiType.php
+++ src/AppBundle/Form/WikiType.php
@@ -3,9 +3,9 @@
 namespace Raddit\AppBundle\Form;
 
 use Raddit\AppBundle\Form\Model\Wiki;
+use Raddit\AppBundle\Form\Type\MarkdownType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +14,7 @@ class WikiType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('title', TextType::class)
-            ->add('body', TextareaType::class)
+            ->add('body', MarkdownType::class)
             ->add('submit', SubmitType::class);
     }
 
