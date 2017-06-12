2ce040038e5a6a595d5cbf36af219938175697ad
Emma <emma1312@protonmail.ch>
2017-06-12 06:38:11 +0200

    404 pages for wiki

--- src/AppBundle/Controller/WikiController.php
+++ src/AppBundle/Controller/WikiController.php
@@ -4,12 +4,14 @@ namespace Raddit\AppBundle\Controller;
 
 use Raddit\AppBundle\Entity\WikiPage;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 final class WikiController extends Controller {
     public function wikiAction(WikiPage $page = null) {
         if (!$page) {
-            // todo
-            throw $this->createNotFoundException('No such page');
+            $response = new Response('', 404);
+
+            return $this->render('@RadditApp/wiki_404.html.twig', [], $response);
         }
 
         return $this->render('@RadditApp/wiki.html.twig', [
