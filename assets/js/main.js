eea408aea164813c2f5f913fecdbcb86befe068f
Emma <emma1312@protonmail.ch>
2017-09-14 22:03:28 +0200

    syntax highlighting

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,33 @@ $('.select2').each(function () {
 $('.confirm-comment-delete').click(function () {
     return confirm(Translator.trans('prompt.confirm_comment_delete'));
 });
+
+$('code[class^="language-"]').each(function () {
+    const nightMode = $('body').hasClass('night-mode');
+    let language = this.className.replace(/.*language-(\S+).*/, "$1");
+
+    if (language === 'html') {
+        language = 'xml';
+    }
+
+    let theme;
+
+    if (nightMode) {
+        theme = import('highlight.js/styles/darkula.css');
+    } else {
+        theme = import('highlight.js/styles/tomorrow.css');
+    }
+
+    Promise.all([
+        import('highlight.js/lib/highlight'),
+        import(`highlight.js/lib/languages/${language}`),
+        theme,
+    ]).then(imports => {
+        const [hljs, definition] = imports;
+
+        hljs.registerLanguage(language, definition);
+        hljs.highlightBlock(this);
+    }).catch(e => {
+        console && console.log(e);
+    });
+});
