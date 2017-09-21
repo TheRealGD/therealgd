3fad191e4f4f40305e26b427b46ef854f7f8e490
Emma <emma1312@protonmail.ch>
2017-09-21 17:01:54 +0200

    set icon w*h / fix update-icons.sh / update icons

--- bin/update-icons.sh
+++ bin/update-icons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installs new icons to src/AppBundle/Resources/views/icons.
+# Installs new icons to src/AppBundle/Resources/views/_icons.
 # These icons should be committed to the git repository. Old icons are not
 # removed.
 
@@ -9,10 +9,11 @@ set -e
 PROJECT_ROOT=$(dirname $(dirname $(realpath $0)))
 PATH="$PROJECT_ROOT/node_modules/.bin:$PATH"
 TEMP=$(mktemp -d)
-OUT="$PROJECT_ROOT/src/AppBundle/Resources/views/icons"
+OUT="$PROJECT_ROOT/src/AppBundle/Resources/views/_icons"
 
 trap 'rm -rf "$TEMP"' EXIT
 
 fontello-cli install --config "$PROJECT_ROOT/fontello.json" --font "$TEMP" --css "$TEMP"
 font-blast "$TEMP/raddit.svg" "$TEMP"
+perl -pi -e's/^(<svg) ?/$1 width="16" height="16" /' "$TEMP/svg/"*.svg
 mv "$TEMP/svg/"*.svg "$OUT"
