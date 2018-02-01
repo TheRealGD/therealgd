6a59b0a5a3995ffb796f8a6aebc9559a4f65d560
Emma <emma1312@protonmail.ch>
2018-02-01 17:47:10 +0100

    minor changes to syntax.js

--- assets/js/syntax.js
+++ assets/js/syntax.js
@@ -2,27 +2,27 @@
 
 import $ from 'jquery';
 
+const languageAliases = {
+    'html': 'xml',
+    'c': 'cpp',
+};
+
 $(function () {
     $('code[class^="language-"]').each(function () {
         const nightMode = $('body').hasClass('night-mode');
-        let language = this.className.replace(/.*language-(\S+).*/, "$1");
 
-        if (language === 'html') {
-            language = 'xml';
+        let language = this.className.replace(/.*language-(\S+).*/, '$1');
+
+        if (languageAliases.hasOwnProperty(language)) {
+            language = languageAliases[language];
         }
 
-        let theme;
-
-        if (nightMode) {
-            theme = import('highlight.js/styles/darkula.css');
-        } else {
-            theme = import('highlight.js/styles/tomorrow.css');
-        }
+        const theme = nightMode ? 'darkula' : 'tomorrow';
 
         Promise.all([
             import('highlight.js/lib/highlight'),
             import(`highlight.js/lib/languages/${language}`),
-            theme,
+            import(`highlight.js/styles/${theme}.css`)
         ]).then(imports => {
             const [hljs, definition] = imports;
 
