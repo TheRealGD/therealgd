aa0a7e1ef0f66866c34f9fdcfa1d77959fcc0f12
Emma <emma1312@protonmail.ch>
2018-02-25 22:35:09 +0100

    redirect to last visited location when logging in

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,10 +3,27 @@
 namespace App\Controller;
 
 use App\Mailer\ResetPasswordMailer;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 final class SecurityController extends AbstractController {
-    public function login(AuthenticationUtils $helper, ResetPasswordMailer $mailer) {
+    use TargetPathTrait;
+
+    public function login(
+        AuthenticationUtils $helper,
+        ResetPasswordMailer $mailer,
+        Request $request
+    ) {
+        // store the last visited location if none exists
+        if (!$this->getTargetPath($request->getSession(), 'main')) {
+            $referer = $request->headers->get('Referer');
+
+            if ($referer && \strpos($referer, $request->getUriForPath('/')) === 0) {
+                $this->saveTargetPath($request->getSession(), 'main', $referer);
+            }
+        }
+
         return $this->render('security/login.html.twig', [
             'can_reset_password' => $mailer->canMail(),
             'error' => $helper->getLastAuthenticationError(),
