57f449f808d86f308f3a58189f47a66bd920b24f
Stepan Generalov <psi.pk.ru@gmail.com>
2018-04-07 02:12:54 +0000

    Enable logger for Controllers to Console/Docker log

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,8 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as BaseAbstractController;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 abstract class AbstractController extends BaseAbstractController {
+    protected $logger = null;
+    public function __construct(LoggerInterface $logger = null) {
+        $this->logger = $logger ?: new NullLoger();
+    }
+
     protected function validateCsrf(string $id, string $token) {
         if (!$this->isCsrfTokenValid($id, $token)) {
             throw new BadRequestHttpException('Invalid CSRF token');
