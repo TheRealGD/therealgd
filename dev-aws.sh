5473a65ef90dd290219f42bd1c9968951ecc8f94
Stepan Generalov <psi.pk.ru@gmail.com>
2018-04-23 09:33:08 +0000

    Don't use sudo in dev-aws

--- dev-aws.sh
+++ dev-aws.sh
@@ -5,7 +5,7 @@ export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`;
 
 erb -T - ./docker-compose.yaml.aws.erb > ./docker-compose.yaml.dev
 
-sudo docker-compose -f ./docker-compose.yaml.dev stop;
-sudo docker-compose -f ./docker-compose.yaml.dev rm -f -v;
-sudo docker-compose -f ./docker-compose.yaml.dev build --force-rm;
-sudo docker-compose -f ./docker-compose.yaml.dev up;
+docker-compose -f ./docker-compose.yaml.dev stop;
+docker-compose -f ./docker-compose.yaml.dev rm -f -v;
+docker-compose -f ./docker-compose.yaml.dev build --force-rm;
+docker-compose -f ./docker-compose.yaml.dev up;
