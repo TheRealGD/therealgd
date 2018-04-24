fd4dd4c0e58121eeb555bcb16902ed4ac5e7165c
Stepan Generalov <psi.pk.ru@gmail.com>
2018-04-24 02:46:55 +0000

    nginx: update to use certbot-auto

--- docker/nginx/scripts/entrypoint.sh
+++ docker/nginx/scripts/entrypoint.sh
@@ -4,4 +4,14 @@
 crontab /certcrontab
 cron
 
-nginx -g 'daemon off;'
+# Launch nginx, if it fails - try to obtain HTTPS & retry once
+nginx -g 'daemon off;'                                     \
+|| (                                                       \
+    echo "Obtaining initial HTTPS Cert"                                                    && \
+    echo "docker-run -ti therealgd_nginx --entrypoint=\"certbot --nginx\""                 && \
+    mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.real                                    && \
+    cp /etc/nginx/nginx.conf.fake /etc/nginx/nginx.conf                                    && \
+    ./certbot-auto --nginx -n --agree-tos --email $ADMIN_EMAIL --domains $REDIRECT_DOMAINS && \
+    killall nginx;                                                                         && \
+    cp /etc/nginx/nginx.conf.real /etc/nginx/nginx.conf                                    && \
+    nginx -g 'daemon off;')
