1bb9be5f5e02ce96bea393da3511ae77e820cf06
Stepan Generalov <psi.pk.ru@gmail.com>
2018-04-25 01:46:24 +0000

    NGINX: fix entrypoint.sh

--- docker/nginx/scripts/entrypoint.sh
+++ docker/nginx/scripts/entrypoint.sh
@@ -12,6 +12,6 @@ nginx -g 'daemon off;'                                     \
     mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.real                                    && \
     cp /etc/nginx/nginx.conf.fake /etc/nginx/nginx.conf                                    && \
     ./certbot-auto --nginx -n --agree-tos --email $ADMIN_EMAIL --domains $REDIRECT_DOMAINS && \
-    killall nginx;                                                                         && \
+    killall nginx                                                                          && \
     cp /etc/nginx/nginx.conf.real /etc/nginx/nginx.conf                                    && \
     nginx -g 'daemon off;')
